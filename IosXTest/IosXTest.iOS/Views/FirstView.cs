b32fd83b7fbd2e8cedb24c87cbe72bab324fdf1c
kurt ziegler <klzig@live.com>
2018-01-18 22:09:11 -0700

    Switched ios from storyboard to xib

--- IosXTest/IosXTest.iOS/Views/FirstView.cs
+++ IosXTest/IosXTest.iOS/Views/FirstView.cs
@@ -1,21 +1,30 @@
-﻿using System;
-using MvvmCross.Binding.BindingContext;
+﻿using MvvmCross.Binding.BindingContext;
 using MvvmCross.iOS.Views;
 using IosXTest.Core.ViewModels;
+using System;
+
+using UIKit;
 
 namespace IosXTest.iOS.Views
 {
-    [MvxFromStoryboard]
     public partial class FirstView : MvxViewController
     {
-        public FirstView(IntPtr handle) : base(handle)
+        public FirstView() : base("FirstView", null)
         {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
         }
 
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
 
+            // Perform any additional setup after loading the view, typically from a nib.
             var set = this.CreateBindingSet<FirstView, FirstViewModel>();
             set.Bind(Label).To(vm => vm.Hello);
             set.Bind(TextField).To(vm => vm.Hello);
