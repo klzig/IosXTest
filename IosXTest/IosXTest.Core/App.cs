4aea35cf770c07785cfb2fb365317e3a78bf9bbe
kurt ziegler <klzig@live.com>
2018-01-18 21:38:15 -0700

    Add project files.

---
+++ IosXTest/IosXTest.Core/App.cs
@@ -0,0 +1,17 @@
+using MvvmCross.Platform.IoC;
+
+namespace IosXTest.Core
+{
+    public class App : MvvmCross.Core.ViewModels.MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<ViewModels.FirstViewModel>();
+        }
+    }
+}
